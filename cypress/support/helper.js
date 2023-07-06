8bc6b6f48a28b2699a5fe9539c5cfab28c27590e
Svitlana Chorna <sv.schwarz1@gmail.com>
2023-07-06 22:58:11 +0300

    request for managing posts are added

--- cypress/support/helper.js
+++ cypress/support/helper.js
@@ -17,21 +17,3 @@ export function register() {
         return response.body;
     });
 }
-
-export function login() {
-
-    const loginData = {
-        email: 'olivier1@mail.com',
-        password: 'testPas',
-    };
-
-    return cy.request('POST', '/login', loginData).then(response => {
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property('accessToken');
-        expect(response.body.user).to.have.property('email');
-        expect(response.body.user).to.have.property('id');
-
-        return response.body;
-    });
-};
-
